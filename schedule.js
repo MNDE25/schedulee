05be797b5ad0b6ae0eb01689f510a4774c2528a8
MNDE25 <mendbayarbyambadorj5@gmail.com>
2024-01-04 19:14:34 +0800

    utgaa avsan

--- schedule.js
+++ schedule.js
@@ -1,3 +1,9 @@
+const todoTasks = [];
+const inProgressTasks = [];
+const stuckTasks = [];
+const doneTasks = [];
+console.log(todoTasks);
+
 function addCardMenu() {
     const add = document.getElementById("CreateModal")
     add.style.display = "flex";
@@ -9,12 +15,17 @@ function hideModal() {
 }
 
 function getValues() {
-const tittleInput = document.getElementById("addCardMenuTittle");
-const describInput = document.getElementById("addCardMenuDesc");
-const statsSelect = document.getElementById("addCardMenuStats");
-const priorSelect = document.getElementById("addCardMenuPrior")
-console.log(tittleInput.value);
-console.log(describInput.value);
-console.log(statsSelect.value);
-console.log(priorSelect.value);
+const tittleInput = document.getElementById("addCardMenuTittle").value;
+const describInput = document.getElementById("addCardMenuDesc").value;
+const statsSelect = document.getElementById("addCardMenuStats").value;
+const priorSelect = document.getElementById("addCardMenuPrior").value;
+todoTasks.push( { tittleInput, describInput, statsSelect, priorSelect });
+resetValues();
+}
+
+function resetValues() {
+document.getElementById("addCardMenuTittle").value="";
+document.getElementById("addCardMenuDesc").value="";
+document.getElementById("addCardMenuStats").value="";
+document.getElementById("addCardMenuPrior").value="";
 }
